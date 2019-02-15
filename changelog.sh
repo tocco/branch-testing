5d0e623d19ea8495c07bc86644008a5eadd0fcc2
Daniel Keller <dkeller@tocco.ch>
2019-02-15 13:29:39 +0100

    chore: changelog script

--- changelog.sh
+++ changelog.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-echo "TEST"
+package=$1
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+last_release_tag=$(git describe --tags --match 'test-'${package}'-tocco@*' --abbrev=0 ${current_branch})
+
+echo "Last release: $last_release_tag"
+
+changelog=$(git log --pretty='%b' ${last_release_tag}..HEAD --grep=''${package}'' --reverse | grep -E '^Changelog:' | awk '{gsub("Changelog:", "- ", $0); print}')
+
+changelog_file="./packages/${package}/changelog.md"
+
+echo "=======CHANGELOG======="
+echo ${last_release_tag} | awk -F '@' '{print $2}' | tee -a "${changelog_file}"
+echo "$changelog" | tee -a "${changelog_file}"
+echo "======================="
+
+
+
+

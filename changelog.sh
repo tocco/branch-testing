90a46ed796b56a17da14e62307d863ddf1c2add4
Daniel Keller <dkeller@tocco.ch>
2019-02-15 14:57:29 +0100

    chore: publish script

--- changelog.sh
+++ changelog.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
-
 package=$1
+
+color_blue=`tput setaf 4`
+color_green=`tput setaf 2`
+color_reset=`tput sgr0`
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 last_release_tag=$(git describe --tags --match 'test-'${package}'-tocco@*' --abbrev=0 ${current_branch})
 
-echo "Last release: $last_release_tag"
+last_version=$(echo ${last_release_tag} | awk -F '@' '{print $2}')
 
 changelog=$(git log --pretty='%b' ${last_release_tag}..HEAD --grep=''${package}'' --reverse | grep -E '^Changelog:' | awk '{gsub("Changelog:", "- ", $0); print}')
 
-changelog_file="./packages/${package}/changelog.md"
 
-echo "=======CHANGELOG======="
-echo ${last_release_tag} | awk -F '@' '{print $2}' | tee -a "${changelog_file}"
-echo "$changelog" | tee -a "${changelog_file}"
-echo "======================="
-
+echo "---------------------"
+echo "info latest release tag: ${color_blue}${last_release_tag} ${color_reset}"
+echo "info latest version: ${color_blue}${last_version}${color_reset}"
+echo -e  "Generated changelog:\n${color_blue}${changelog}${color_reset}"
 
 
+read -p "question New version: : " new_version
+echo "publishing ${package} with version ${new_version}"
 
+cd packages/${package}
+yarn publish --new-version ${new_version}
+
+
+changelog_file="./changelog.md"
+
+echo "${color_green} Pulished! Changelog was added to changelog.md and needs to be amend commited!${color_reset}"
+echo "---------------------"

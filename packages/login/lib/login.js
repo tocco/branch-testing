f85feb1c75427fb681d7f02b937cee629a6c2295
Daniel Keller <dkeller@tocco.ch>
2019-02-27 09:08:05 +0100

    feat(tocco-login): add new feature
    Changelog: Add new feature xy

--- packages/login/lib/login.js
+++ packages/login/lib/login.js
@@ -3,16 +3,5 @@
 module.exports = login;
 
 function login() {
-    // Version 3.0.2 Code
-    // Hotfix 1 (215)
-    // Hotfix 1 (216)
-    // Hotfix Problem Y
-    // Feat 1
-    // Feat 2
-    // Feat 3
-    // Feat 4
-    // Feat 5
-    // Feat 6
-   // Feat 7
-   // Feat 8
+  // add new feature
 }

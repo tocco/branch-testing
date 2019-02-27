19215fa02325eb2a396173caabc4fdb547027725
Daniel Keller <dkeller@tocco.ch>
2019-02-27 09:09:06 +0100

    feat(tocco-merge): add new merge feature
    Changelog: Add new merge feature!

--- packages/merge/lib/merge.js
+++ packages/merge/lib/merge.js
@@ -3,14 +3,5 @@
 module.exports = merge;
 
 function merge() {
-    // Version 1.0.2 Code
-  // Feat 1
-  //Feat 2
-  //Feat 2
-  //Feat 4
-  //Feat 5
-  //Feat 6
-  //Feat 7
-  //Feat 7
-  //Feat 8
+  // add new merge feature
 }
